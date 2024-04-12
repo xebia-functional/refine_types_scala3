b9ab01ca0487e888d6ca6000cb9eee8bbebdcf2c
david <da.gi.me@protonmail.com>
2024-04-12 23:46:13 +0200

    Correct prefix for Raw Classes

--- 07-presentation/src/main/scala/dagmendez/presentation/B_RawClasses.scala
+++ 07-presentation/src/main/scala/dagmendez/presentation/B_RawClasses.scala
@@ -1,14 +1,14 @@
 package dagmendez.presentation
 
-object A_RawClasses extends App:
+object B_RawClasses extends App:
   
   // Closest to traditional Java
   class DummyDNI(dni: String):
     override def toString: String = s"DummyDNI($dni)"
   end DummyDNI
   
-  List[DummyDNI](
-    new DummyDNI("00000001A"),
+  Vector[DummyDNI](
+    new DummyDNI("00000001A"), 
     new DummyDNI("ABCDEFGH0")
   ).foreach(println)
 
@@ -16,7 +16,6 @@ object A_RawClasses extends App:
   case class DNI(numero: String, letra: Char):
     def value: String = s"$numero-$letra"
 
-  List[Any](
-    DNI("12345678", 'A').value,
-    DNI("0", 'f').value
-  ).foreach(println)
+  Vector[DNI](DNI("12345678", 'A'), DNI("0", 'f'))
+    .map(_.value)
+    .foreach(println)
