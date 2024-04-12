1f360be1182565acdd3409e88b575a9c1c5e5e02
david <da.gi.me@protonmail.com>
2024-04-13 00:00:26 +0200

    Fix Raw Classes and Value Classes. Make printing lambdas more explicit to also explain them

--- 07-presentation/src/main/scala/dagmendez/presentation/B_RawClasses.scala
+++ 07-presentation/src/main/scala/dagmendez/presentation/B_RawClasses.scala
@@ -10,12 +10,12 @@ object B_RawClasses extends App:
   Vector[DummyDNI](
     new DummyDNI("00000001A"), 
     new DummyDNI("ABCDEFGH0")
-  ).foreach(println)
+  ).foreach(dummyDNI => println(dummyDNI))
 
   // Basic Vanilla Scala
   case class DNI(numero: String, letra: Char):
     def value: String = s"$numero-$letra"
 
   Vector[DNI](DNI("12345678", 'A'), DNI("0", 'f'))
-    .map(_.value)
-    .foreach(println)
+    .map(dni => dni.value)
+    .foreach(dniValue => println(dniValue))
---
+++ 07-presentation/src/main/scala/dagmendez/presentation/C_ValueClasses.scala
@@ -0,0 +1,23 @@
+package dagmendez.presentation
+
+object C_ValueClasses:
+
+  case class DniNumber(number: String) extends AnyVal
+  case class DniControlChar(character: Char) extends AnyVal
+
+  case class DNI(numero: DniNumber, letra: DniControlChar):
+    def value: String = s"${numero.number}-${letra.character}"
+
+  val dnis =  Vector[DNI](
+    DNI(DniNumber("12345678"), DniControlChar('A')),
+    DNI(DniNumber("0"), DniControlChar('f')),
+    DNI(DniNumber("Hola"), DniControlChar('0'))
+  )
+
+  @main def printDNI(): Unit = 
+    // Python-ish way if writing lambda functions, but with a line break
+    dnis
+      .map: dni => 
+        dni.value
+      .foreach: dniValue =>
+        println(dniValue)
