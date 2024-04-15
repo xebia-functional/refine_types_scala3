a3c1598d0a5555e9d6df64f9901cd42408473c7e
david <da.gi.me@protonmail.com>
2024-04-16 00:38:19 +0200

    Delete I_Iron and update H_Iron

--- 07-presentation/src/main/scala/dagmendez/presentation/H_Iron.scala
+++ 07-presentation/src/main/scala/dagmendez/presentation/H_Iron.scala
@@ -2,24 +2,53 @@ package dagmendez.presentation
 
 import io.github.iltotore.iron.constraint.any.DescribedAs
 import io.github.iltotore.iron.constraint.string.Match
+import io.github.iltotore.iron.constraint.numeric.{Positive, LessEqual}
+import io.github.iltotore.iron.constraint.any.In
 import io.github.iltotore.iron.{:|, RefinedTypeOps, autoRefine}
-
+import scala.util.control.NoStackTrace
 object H_Iron:
 
-  type ValidDniNumber   = Match["[0-9]{8}"] DescribedAs "El número ha de contener 8 caracteres numéricos"
-  opaque type DniNumber = String :| ValidDniNumber
-  object DniNumber extends RefinedTypeOps[String, ValidDniNumber, DniNumber]
+  case class FormatError(reason: String) extends Exception(reason) with NoStackTrace
 
-  type ValidDniControlLetter   = Match["[ABCDEFGHJKLMNPQRSTWXYZ]{1}"] DescribedAs "Letra de control no válida"
-  opaque type DniControlLetter = String :| ValidDniControlLetter
-  object DniControlLetter extends RefinedTypeOps[String, ValidDniControlLetter, DniControlLetter]
+  opaque type ValidNumber = Positive & LessEqual[99999999] DescribedAs "Maximum amount of numbers is 8."
+  opaque type DniNumber   = Int :| ValidNumber
+  object DniNumber extends RefinedTypeOps[Int, ValidNumber, DniNumber]
+  extension (number: DniNumber) inline def value: Int = number
 
-  case class DNI(numero: DniNumber, letra: DniControlLetter):
-    def value: String = s"${numero.toString}-${letra.toString}"
+  opaque type ValidLetter =
+    In[("A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "W", "X", "Y", "Z")] DescribedAs
+      "Invalid control letter."
+  opaque type DniLetter = String :| ValidLetter
+  object DniLetter extends RefinedTypeOps[String, ValidLetter, DniLetter]
+  extension (letter: DniLetter) inline def value: String = letter
 
-  @main def ironRun: Unit =
+  case class DNI(number: DniNumber, letter: DniLetter):
+    def value: String =
+      val formatLeadingZeros = String.format("%08d", number.value)
+      s"$formatLeadingZeros-$letter"
+
+  object DNI:
+    def parse(number: DniNumber, letter: DniLetter): Either[FormatError, DNI] =
+      Either.cond(
+        letter.value == controlDigit(number.value % 23),
+        DNI(number, letter),
+        FormatError("Verify the DNI. Control letter does not match the number.")
+      )
+  end DNI
+
+  def main(args: Array[String]): Unit =
     Vector(
-      // Descomentar para ver las trazas de Iron en tiempo de compilación
-      // DNI(DniNumber("12345"), DniControlLetter("O")),
-      DNI(DniNumber("12345678"), DniControlLetter("D"))
-    ).map(_.value).foreach(println)
+      // FormatError: Verify the DNI. Control letter does not match the number.
+      DNI.parse(DniNumber(1), DniLetter("A")),
+      // Won't compile due to type checking
+      // DNI.parse(DniNumber("R"), DniLetter("00000001")),
+      // compile error: Maximum amount of numbers is 8.
+      // DNI.parse(DniNumber(123456789), DniLetter("A")),
+      // compile error: Invalid control letter.
+      // DNI.parse(DniNumber(12345678), DniLetter("U")),
+      // FormatError: Verify the DNI. Control letter does not match the number.
+      DNI.parse(DniNumber(12345678), DniLetter("B")),
+      // Valid DNIs
+      DNI.parse(DniNumber(1), DniLetter("R")),
+      DNI.parse(DniNumber(12345678), DniLetter("Z"))
+    ).map(either => either.map(dni => dni.value)).foreach(println)
