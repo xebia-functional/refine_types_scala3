f1105a3a421fc6fab99acb7f48a2719e917ce3a3
david <da.gi.me@protonmail.com>
2024-04-15 02:35:40 +0200

    Update Value Classes with Error Handling

---
+++ 07-presentation/src/main/scala/dagmendez/presentation/E_ValueClassesWithErrorHandling.scala
@@ -0,0 +1,58 @@
+package dagmendez.presentation
+
+import scala.util.control.NoStackTrace
+
+object E_ValueClassesWithErrorHandling:
+
+  case class FormatError(reason: String) extends Exception(reason) with NoStackTrace
+
+  case class DniNumber(number: String)
+  object DniNumber:
+    def parse(number: String): Either[FormatError, DniNumber] =
+      Either.cond(
+        number.length == 8 && number.forall(_.isDigit),
+        DniNumber(number),
+        FormatError("The leading 8 characters must be digits.")
+      )
+  end DniNumber
+
+  case class DniLetter(letter: String)
+  object DniLetter:
+    def parse(letter: String): Either[FormatError, DniLetter] =
+      Either.cond(
+        controlDigit.values.toVector.contains(letter),
+        DniLetter(letter),
+        FormatError("Invalid control letter.")
+      )
+  end DniLetter
+
+  case class DNI(number: DniNumber, letter: DniLetter)
+  object DNI:
+    def parse(number: Either[FormatError, DniNumber], letter: Either[FormatError, DniLetter]): Either[FormatError, DNI] =
+      for
+        n <- number
+        l <- letter
+        dni <- Either.cond(
+                 l.letter == controlDigit(n.number.toInt % 23),
+                 DNI(n, l),
+                 FormatError("Verify the DNI. Control letter does not match the number.")
+               )
+      yield dni
+  end DNI
+
+  def main(args: Array[String]): Unit =
+    Vector(
+      // FormatError: Verify the DNI. Control letter does not match the number.
+      DNI.parse(DniNumber.parse("00000001"), DniLetter.parse("A")),
+      // FormatError: The leading 8 characters must be digits.
+      DNI.parse(DniNumber.parse("R"), DniLetter.parse("00000001")),
+      // FormatError: The leading 8 characters must be digits.
+      DNI.parse(DniNumber.parse("123BCD78"), DniLetter.parse("A")),
+      // FormatError: Invalid control letter.
+      DNI.parse(DniNumber.parse("12345678"), DniLetter.parse("U")),
+      // FormatError: Verify the DNI. Control letter does not match the number.
+      DNI.parse(DniNumber.parse("12345678"), DniLetter.parse("B")),
+      // Valid DNIs
+      DNI.parse(DniNumber.parse("00000001"), DniLetter.parse("R")),
+      DNI.parse(DniNumber.parse("12345678"), DniLetter.parse("Z"))
+    ).foreach(println)
