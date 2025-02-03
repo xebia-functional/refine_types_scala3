ebd6ed036abe3b94104d300a706051ce466d7f2f
David <david.a.gil@proton.me>
2025-02-03 14:11:08 +0100

    Fix broken implementation of NIE. Changes type for Number to avoid issues printing IDs that are led by zeros. Simplifies the parse method on object ID.

--- 03-neotype/src/main/scala/dagmendez/neotype/NeoType.scala
+++ 03-neotype/src/main/scala/dagmendez/neotype/NeoType.scala
@@ -12,22 +12,27 @@ import neotype._
 object NeoType:
 
   type ValidNumber = ValidNumber.Type
-  object ValidNumber extends Newtype[Long]:
-    override inline def validate(input: Long): Boolean | String =
-      input match
-        case negative if negative <= 0L => s"$input is not positive. PositiveNumber must be positive"
-        case short if short > 99999999L => s"$input is too long. Maximum amount of numbers is 8"
-        case _                          => true
+  object ValidNumber extends Newtype[String]:
+    override inline def validate(input: String): Boolean | String =
+      if input.exists(_.isLetter) then "Illegal amount of letters in the ID"
+      else
+        input.toLong match
+          case negative if negative <= 0L => s"$input is not positive. PositiveNumber must be positive"
+          case short if short > 99999999L => s"$input is too long. Maximum amount of numbers is 8"
+          case _                          => true
 
   type ValidControlLetter = ValidControlLetter.Type
-  object ValidControlLetter extends Newtype[Char]:
-    override inline def validate(input: Char): Boolean | String = controlLetter.values.toSeq.contains(input)
+  object ValidControlLetter extends Newtype[String]:
+    override inline def validate(input: String): Boolean | String =
+      if controlLetter.values.toSeq.contains(input.toCharArray.head) then true
+      else s"$input is not a valid control letter"
 
   object ValidID extends Newtype[(ValidNumber, ValidControlLetter)]:
     override def validate(input: (ValidNumber, ValidControlLetter)): Boolean | String =
-      val number = input._1.unwrap // Extracts the underlying Long
-      val letter = input._2.unwrap // Extracts the underlying Char
-      letter == controlLetter(number.toInt % 23) // Validates the control char against the number
+      val number = input._1.unwrap.toInt            // Extracts the underlying Int
+      val letter = input._2.unwrap.toCharArray.head // Extracts the underlying Char
+      if letter == controlLetter(number % 23) then true // Validates the control letter against the number
+      else s"The control letter '$letter' is not matching the number"
 
   /**
    * The NIE initial letters are defined as an Enum to leverage the method "ordinal". The letter should be replaced by number like this:
@@ -42,9 +47,14 @@ object NeoType:
 
   class NIE(number: ValidNumber, letter: ValidControlLetter):
     override def toString: String =
-      val nieLetter     = NieLetter.fromOrdinal(number.unwrap.toString.head.toString.toInt)
-      val nieNumber     = number.unwrap.toString.tail
-      val controlLetter = letter.unwrap
+      // Head (char) is cast to String before casting it to Int to get the proper index.
+      // For example - Char: '0'.toInt = 48
+      // For example - String: "0".toInt = 0
+      val nieLetterAsNumber: Int = number.unwrap.head.toString.toInt
+      // Only 0, 1 and 2 are valid ordinals
+      val nieLetter: NieLetter  = NieLetter.fromOrdinal(nieLetterAsNumber)
+      val nieNumber: String     = number.unwrap.tail
+      val controlLetter: String = letter.unwrap
       s"$nieLetter-$nieNumber-$controlLetter"
 
   class DNI(number: ValidNumber, letter: ValidControlLetter):
@@ -53,8 +63,8 @@ object NeoType:
   object ID:
     def parse(input: String): Either[String, DNI | NIE] =
 
-      // Allows for introducing DNI with or without dash as input.
-      val withoutDash = input.replace("-", "")
+      // Allows for introducing an ID with or without dash as input.
+      val withoutDash: String = input.replace("-", "")
 
       // Inputs with length smaller or bigger than 9 are invalid by definition (once the dash is removed).
       if withoutDash.length != 9 then Left("Valid ID must be 9 characters long by counting only the numbers and letters.")
@@ -64,39 +74,43 @@ object NeoType:
         // Splits the numbers from the tailing character.
         val (number, letter) = withoutDash.splitAt(8)
 
-        // We check if the ID is a NIE or a DNI
-        val maybeNie: Option[NieLetter] =
-          if number.head.isDigit then None
-          else Some(NieLetter.valueOf(number.head.toString.toUpperCase)) // Allows for the character to be both upper and lower case.
+        // If the first character is a letter, then the ID is a NIE, otherwise is a DNI
+        val isNIE: Boolean = number.head.isLetter
 
-        // We build the number. If it is a NIE, we must transform the first letter into a digit
-        val completeNumber =
-          if maybeNie.isEmpty then number.toLong
-          else s"${maybeNie.get.ordinal}${number.tail}".toLong
+        val completeNumber: String =
+          // If the ID is a NIE, then the first letter must be transformed into a number before we continue processing.
+          if isNIE then s"${NieLetter.valueOf(number.head.toString.toUpperCase).ordinal}${number.tail}"
+          // Otherwise, we just continue with the number
+          else number
 
         // Chains the computations of validations in a for comprehension.
         for
           number <- ValidNumber.make(completeNumber)
-          letter <- ValidControlLetter.make(letter.toUpperCase.head) // Allows for the character to be both upper and lower case.
-          id     <- ValidID.make(number, letter)                     // Validates if the control character matches the ID number
+          // Allows for the character to be both upper and lower case.
+          letter <- ValidControlLetter.make(letter.toUpperCase)
+          // Validates if the control character matches the ID number
+          id <- ValidID.make(number, letter)
         yield
-          if maybeNie.isEmpty
-          then DNI(id.unwrap._1, id.unwrap._2)
-          else NIE(id.unwrap._1, id.unwrap._2)
+          if isNIE then NIE(id.unwrap._1, id.unwrap._2)
+          else DNI(id.unwrap._1, id.unwrap._2)
 
   @main
   def validate(): Unit =
 
     println("""
-        | *---------------*
-        | | DNI validator |
-        | *---------------*
+        | *----------------------*
+        | | Spanish ID validator |
+        | *----------------------*
         |
-        | Introduce any DNI. For example:
+        | Introduce any ID. For example:
         | - 12345678-Z
         | - 12345678-z
         | - 12345678Z
         | - 12345678z
+        | - Y-2345678-Z
+        | - Y2345678Z
+        | - y-2345678-z
+        | - y2345678z
         |
         | write QUIT, quit, Q or q to exit the program
         |
@@ -107,7 +121,9 @@ object NeoType:
       val exitCommand = Set("QUIT", "Q", "quit", "q")
       if exitCommand.contains(userInput) then ()
       else {
-        ID.parse(userInput).foreach(id => println(s"\n$id is valid.\n"))
+        ID.parse(userInput) match
+          case Right(id)   => println(s"$id is a valid ID")
+          case Left(error) => println(s"$userInput is not a valid ID. Reason: $error")
         loop()
       }
     loop()
